89546c6c772e5a73ff79deddb170749657a51484
amir daaee <amir.daaee@gmail.com>
2022-01-27 16:50:20 +0330

    skip domains capability

--- script.py
+++ script.py
@@ -8,15 +8,26 @@ import aiodns
 import aiohttp
 from tqdm.asyncio import tqdm
 
+
+def list_from_file(path):
+    with open(path, encoding='utf8') as f_:
+        return [x for x in f_.read().split('\n') if len(x)]
+
+
 ROOT_PATH = Path(os.path.dirname(os.path.abspath(__file__)))
 GOOGLE_IPS_PATH = ROOT_PATH / 'google-ipv4.txt'
 DOMAINS_PATH = ROOT_PATH / 'data/domains.txt'
 GOOGLE_OPEN_PATH = ROOT_PATH / 'data/google-open.json'
 GOOGLE_BLOCK_PATH = ROOT_PATH / 'data/google-block.json'
+EXCLUDED_DOMAINS_PATH = ROOT_PATH / 'data/skip.txt'
 
 DNS_RESOLVER = aiodns.DNSResolver(nameservers=['8.8.8.8', '8.8.4.4'])
-with open(GOOGLE_IPS_PATH, encoding='utf8') as google_ipv4_file:
-    GOOGLE_IPS = [x for x in google_ipv4_file.read().split('\n') if len(x)]
+GOOGLE_IPS = set(list_from_file(GOOGLE_IPS_PATH))
+
+if EXCLUDED_DOMAINS_PATH.is_file():
+    EXCLUDED_DOMAINS = set(list_from_file(EXCLUDED_DOMAINS_PATH))
+else:
+    EXCLUDED_DOMAINS = set()
 
 
 async def async_script():
@@ -27,10 +38,6 @@ async def async_script():
         if isinstance(data, list):
             data_ = list(data_)
         return data_
-
-    def load_data():
-        with open(DOMAINS_PATH) as f_:
-            return f_.read().split('\n')
 
     async def resolve(domain):
         try:
@@ -47,20 +54,24 @@ async def async_script():
                 return True
         return False
 
-    async def is_blocked(domain):
+    async def is_open(domain):
         async with aiohttp.ClientSession(
                 timeout=aiohttp.ClientTimeout(total=60),
                 raise_for_status=False) as session:
             try:
                 async with session.get('https://' + domain) as resp:
-                    if resp.status == 403:
+                    if resp.status != 403:
                         return domain
             except (aiohttp.ClientError, asyncio.TimeoutError):
                 pass
         return None
 
     #  ..........
-    domains = load_data()
+    domains = {
+        x
+        for x in list_from_file(DOMAINS_PATH)
+        if not any(map(x.endswith, EXCLUDED_DOMAINS))
+    }
     print('provided domains:', len(domains))
     #  ..........
     domains = set(domains)
@@ -68,17 +79,16 @@ async def async_script():
     #  ..........
     tasks = {resolve(dom) for dom in domains}
     print('resolving domains...')
-    domains = _drop_nan(await tqdm.gather(*tasks))
+    domains = _drop_nan(set(await tqdm.gather(*tasks)))
     print('domains resolved:', len(domains))
     #  ..........
     domains = {x[0] for x in domains if is_google(x[1])}
     print('google backed domains:', len(domains))
     #  ..........
-    tasks = {is_blocked(dom) for dom in domains}
+    tasks = {is_open(dom) for dom in domains}
     print('testing google domains...')
-    blocked_domains = _drop_nan(await tqdm.gather(*tasks))
-    open_domains = domains.difference(blocked_domains)
-    print('total tested domains:', len(open_domains) + len(blocked_domains))
+    open_domains = _drop_nan(set(await tqdm.gather(*tasks)))
+    blocked_domains = domains - open_domains
     print('open domains:', len(open_domains))
     print('blocked domains:', len(blocked_domains))
     #  ..........
